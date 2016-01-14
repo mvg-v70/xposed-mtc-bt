233e8dc4695ef2325d38048d806d2cd8dd99612e
mvg-v70 <gusev-mv@yandex.ru>
2016-01-14 22:06:37 +0300

    version 1.3

--- src/com/mvgv70/xposed_mtc_bt/Main.java
+++ src/com/mvgv70/xposed_mtc_bt/Main.java
@@ -2,8 +2,19 @@ package com.mvgv70.xposed_mtc_bt;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -13,7 +24,12 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class Main implements IXposedHookLoadPackage {
 	
   private final static int NUMBER_LEN = 10;
-  private final static String TAG = "xposed-mtc-bt";  
+  private final static String TAG = "xposed-mtc-bt";
+  private static Activity btActivity;
+  private static boolean phoneBookSorted = false;
+  private static Object phonebookFragment;
+  private static keyboardDialog kbDialog = null;
+  private static List<Character> enabledChars = new ArrayList<Character>();
 		
   @Override
   public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
@@ -39,7 +55,7 @@ public class Main implements IXposedHookLoadPackage {
       protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
         Log.d(TAG,"getNameOfNumbers");
         @SuppressWarnings("unchecked")
-		List<String> phoneBook = (List<String>)XposedHelpers.getObjectField(param.thisObject, "phoneBookList");
+        List<String> phoneBook = (List<String>)XposedHelpers.getObjectField(param.thisObject, "phoneBookList");
         String phoneNumber = (String)param.args[0];
         String contact_name = findNumber(phoneBook,phoneNumber);
         return contact_name;
@@ -47,12 +63,42 @@ public class Main implements IXposedHookLoadPackage {
     };
     
     // BlueToothActivity.updatePhoneBookFirstChar()
-    XC_MethodHook updatePhoneBookFirstChar = new XC_MethodHook() {
+    XC_MethodReplacement updatePhoneBookFirstChar = new XC_MethodReplacement() {
         
       @Override
-      protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-        Log.d(TAG,"updatePhoneBookFirstChar");
-        XposedHelpers.callMethod(param.thisObject, "assortPhoneBook");
+      protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+        Log.d(TAG,"updatePhoneBookFirstChar, phoneBookSorted="+phoneBookSorted);
+        btActivity = (Activity)param.thisObject;
+        if (!phoneBookSorted)
+          XposedHelpers.callMethod(param.thisObject, "assortPhoneBook");
+        @SuppressWarnings("unchecked")
+        List<String> phoneBookList = (List<String>)XposedHelpers.getObjectField(param.thisObject, "phoneBookList");
+        List<Character> phoneBookFirstChar = new ArrayList<Character>();
+        // перебираем контакты в телефонной книге
+        Character ch;
+        enabledChars.clear();
+        for (String line : phoneBookList)
+        {
+          // первый символ имени абонента
+          ch = Character.toUpperCase(line.charAt(0));
+          // добавл€ем символ в список
+          phoneBookFirstChar.add(ch);
+          if (!enabledChars.contains(ch)) enabledChars.add(ch);
+        }
+        // устанавливаем список с первыми буквами
+        XposedHelpers.setObjectField(param.thisObject, "phoneBookFirstChar", phoneBookFirstChar);
+        Log.d(TAG,"updatePhoneBookFirstChar OK");
+        return null;
+      }
+    };
+    
+    // BlueToothActivity.onDestroy()
+    XC_MethodHook onDestroy = new XC_MethodHook() {
+        
+      @Override
+      protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        Log.d(TAG,"onDestroy");
+        phoneBookSorted = false;
       }
     };
     
@@ -63,7 +109,7 @@ public class Main implements IXposedHookLoadPackage {
       protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
         Log.d(TAG,"assortPhoneBook");
         @SuppressWarnings("unchecked")
-		List<String> phoneBookList = (List<String>)XposedHelpers.getObjectField(param.thisObject, "phoneBookList");
+        List<String> phoneBookList = (List<String>)XposedHelpers.getObjectField(param.thisObject, "phoneBookList");
         Log.d(TAG,"phoneBookList.size="+phoneBookList.size());
         if (phoneBookList.size() == 0) return null;
         // отсортированный список
@@ -98,7 +144,138 @@ public class Main implements IXposedHookLoadPackage {
         }
         // устанавливаем отсортированный список
         XposedHelpers.setObjectField(param.thisObject, "phoneBookList", phoneBookListSorted);
+        phoneBookSorted = true;
+        Log.d(TAG,"sorted="+phoneBookSorted);
         return null;
+      }
+    };
+    
+    // Ui1.postKeyboadrShow()
+    XC_MethodReplacement postKeyboadrShow = new XC_MethodReplacement() {
+        
+      @Override
+      protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+        Log.d(TAG,"postKeyboadrShow");
+        Activity mActivity = (Activity)XposedHelpers.getObjectField(param.thisObject, "mActivity");
+        phonebookFragment = XposedHelpers.getObjectField(param.thisObject, "phonebookFragment");
+        TableLayout view = new TableLayout(mActivity);
+        // фонова€ картинка
+        Resources res = btActivity.getResources();
+        int button_bkgnd_id = res.getIdentifier("keyboardbtn", "drawable", btActivity.getPackageName());
+        // строки
+        for (int row=0; row < 6; row++)
+        {
+          TableRow tableRow = new TableRow(mActivity);
+          tableRow.setGravity(Gravity.CENTER);
+          tableRow.setLayoutParams(new TableLayout.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
+          // столбцы
+          for (int col=0; col < 5; col++)
+          {
+            TextView button = new TextView(mActivity);
+            int index = col+row*5;
+            char ch = decodeChar(index);
+            button.setText(String.valueOf(ch));
+            button.setGravity(Gravity.CENTER);
+            button.setBackgroundResource(button_bkgnd_id);
+            button.setEnabled(ch != ' ');
+            button.setClickable(ch != ' ');
+            button.setOnClickListener(searchClick);
+            TableRow.LayoutParams buttonParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
+            buttonParams.setMargins(10, 10, 10, 10);
+            button.setLayoutParams(buttonParams);
+            // добавл€ем кнопку в строку
+            tableRow.addView(button);
+          }
+          // добавл€ем строку кнопок
+          view.addView(tableRow);
+        }
+        // диалог
+        kbDialog = new keyboardDialog(mActivity);
+        kbDialog.setView(view, 0, 0, 0, 0);
+        kbDialog.setCancelable(true);
+        kbDialog.show();
+        return null;
+      }
+    };
+    
+    // com.microntek.bluetooth.ui.PhonebookFragment$PhoneBookAdapter.getView(int,View,ViewGroup)
+    XC_MethodReplacement getView = new XC_MethodReplacement() {
+        
+      @Override
+      protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+    	int position = (int)param.args[0];
+    	View view = (View)param.args[1];
+    	Resources res = btActivity.getResources();
+        // inflate
+    	if (view == null)
+    	{
+    	  int layout_id = res.getIdentifier("pblist", "layout", btActivity.getPackageName());
+    	  Object mInflater = XposedHelpers.getObjectField(param.thisObject, "mInflater");
+    	  view = (View)XposedHelpers.callMethod(mInflater, "inflate", layout_id, null);
+    	}
+    	// текуща€ строка
+    	String curRow = (String)XposedHelpers.callMethod(param.thisObject, "getItem", position);
+    	Character curChar = ' ';
+        String curName = "";
+        String curNumber = "";
+        String[] curValues = curRow.split(Pattern.quote("^"));
+        if (curValues.length >= 2)
+        {
+          curName = curValues[0];
+          curNumber = curValues[1];
+          if (!curName.isEmpty()) curChar = Character.toUpperCase(curName.charAt(0)); 
+        }
+        Log.d(TAG,curChar+": curName="+curName+", curNumber="+curNumber);
+    	// предыдуща€ строка
+    	Character prevChar = ' ';
+    	String prevName = "";
+    	if (position > 0)
+    	{
+    	  String prevRow = (String)XposedHelpers.callMethod(param.thisObject, "getItem", position-1);
+    	  String[] prevValues = prevRow.split(Pattern.quote("^"));
+          if (prevValues.length >= 2)
+          {
+            prevName = prevValues[0];
+            if (!prevName.isEmpty()) prevChar = Character.toUpperCase(prevName.charAt(0));
+          }
+          Log.d(TAG,prevChar+": prevName="+prevName);
+    	}
+        // отображаемые элементы
+        int char_id = res.getIdentifier("tv_firstchar", "id", btActivity.getPackageName());
+        int name_id = res.getIdentifier("tv_friendname", "id", btActivity.getPackageName());
+        int number_id = res.getIdentifier("tv_friendnumber", "id", btActivity.getPackageName());
+        int sep_id = res.getIdentifier("iv_sep", "id", btActivity.getPackageName());
+        int pbbg_id = res.getIdentifier("iv_pbbg", "id", btActivity.getPackageName());
+        // view
+        TextView charView = (TextView)view.findViewById(char_id);
+        TextView nameView = (TextView)view.findViewById(name_id);
+        TextView numberView = (TextView)view.findViewById(number_id);
+        View sepView = view.findViewById(sep_id);
+        View pbbgView = view.findViewById(pbbg_id);
+        // устанавливаем значени€
+        charView.setText(curChar.toString());
+        nameView.setText(curName);
+        numberView.setText(curNumber);
+        // изменилс€ первый символ
+        if (curChar.equals(prevChar))
+          charView.setVisibility(View.INVISIBLE);
+        else
+          charView.setVisibility(View.VISIBLE);
+        // изменилось им€ абонента
+        if (curName.equals(prevName))
+          sepView.setVisibility(View.INVISIBLE);
+        else
+          sepView.setVisibility(View.VISIBLE);
+        // выделение строки
+        int selected = XposedHelpers.getIntField(param.thisObject, "seleteItem");
+        int resource_id;
+        if (position != selected)
+          resource_id = res.getIdentifier("list_selector", "drawable", btActivity.getPackageName());
+        else
+          resource_id = res.getIdentifier("history_listbg", "drawable", btActivity.getPackageName());
+        pbbgView.setBackgroundResource(resource_id);
+    	// return
+    	return view;
       }
     };
     
@@ -108,8 +285,21 @@ public class Main implements IXposedHookLoadPackage {
     XposedHelpers.findAndHookMethod("com.microntek.bluetooth.BlueToothActivity", lpparam.classLoader, "getNameOfNumbers", String.class, getNameOfNumbers);
     XposedHelpers.findAndHookMethod("com.microntek.bluetooth.BlueToothActivity", lpparam.classLoader, "assortPhoneBook", assortPhoneBook);
     XposedHelpers.findAndHookMethod("com.microntek.bluetooth.BlueToothActivity", lpparam.classLoader, "updatePhoneBookFirstChar", updatePhoneBookFirstChar);
+    XposedHelpers.findAndHookMethod("com.microntek.bluetooth.BlueToothActivity", lpparam.classLoader, "onDestroy", onDestroy);
+    XposedHelpers.findAndHookMethod("com.microntek.bluetooth.ui.PhonebookFragment$PhoneBookAdapter", lpparam.classLoader, "getView", int.class, View.class, ViewGroup.class, getView);
+    // ищем используемый Ui
+    for (int i=1; i<=5; i++)
+    {
+      try
+      {
+        XposedHelpers.findAndHookMethod("com.microntek.bluetooth.ui.Ui"+i, lpparam.classLoader, "postKeyboadrShow", postKeyboadrShow);
+        Log.d(TAG,"Ui"+i+" detected...");
+        break;
+      }
+      catch (Error e) {}
+    }
     Log.d(TAG,"com.microntek.bluetooth OK");
-  }
+ }
   
   // поиск номера в списке контактов
   private String findNumber(List<String> phoneBook, String number)
@@ -148,6 +338,50 @@ public class Main implements IXposedHookLoadPackage {
     Log.d(TAG,"contact_name -> "+result);
     // результат
     return result;
+  }
+  
+  public View.OnClickListener searchClick = new View.OnClickListener() 
+  {
+    public void onClick(View v) 
+    {
+      Log.d(TAG,"search button press");
+      try
+      {
+        TextView view = (TextView)v;
+        Log.d(TAG,""+view.getText());
+        XposedHelpers.callMethod(phonebookFragment, "search", view.getText().charAt(0));
+        Log.d(TAG,"search OK");
+        if (kbDialog != null) kbDialog.dismiss();
+        Log.d(TAG,"dismiss OK");
+      }
+      catch (Exception e)
+      {
+        Log.e(TAG,"search exception: "+e.getMessage());
+        if (kbDialog != null) kbDialog.dismiss();
+      }
+    }
+  };
+
+  private static char decodeChar(int index)
+  {
+    if (index < enabledChars.size())
+      return enabledChars.get(index);
+    else
+      return ' ';
+  }
+    
+  private class keyboardDialog extends AlertDialog
+  {
+    public keyboardDialog(Context context)
+    {
+      super(context);
+    }
+    
+    @Override
+    public void onStop()
+    {
+      kbDialog = null;
+    }
   }
 
 }
